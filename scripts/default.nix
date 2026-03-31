cc95b7a7a10c168760c48a5863246bc3fa93f5f2
ItsMagick <magick930@googlemail.com>
2026-03-31 16:20:08 +0200

    - removed unused nix scripts - removed duplicate hyprland settings in home.nix

--- scripts/default.nix
+++ scripts/default.nix
@@ -2,11 +2,7 @@
 
 {
   home.packages = [
-    (import ./wall-picker.nix { inherit pkgs; })
-    (import ./theme-toggle.nix { inherit pkgs; })
-    (import ./theme-menu.nix { inherit pkgs; })
     (import ./window-title.nix { inherit pkgs; })
     (import ./lock.nix {inherit pkgs; })
-    (import ./rofi-nm.nix {inherit pkgs; })
   ];
 }
--- scripts/rofi-nm.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }:
-
-pkgs.writeShellScriptBin "rofi-network" ''
-  # Use networkmanager_dmenu with your existing Rofi theme
-  # We use the -theme-str to make it look like a dropdown
-  ${pkgs.networkmanager_dmenu}/bin/networkmanager_dmenu \
-    -conf /dev/null \
-    -theme-str 'window { width: 20%; } listview { lines: 10; }'
-''
