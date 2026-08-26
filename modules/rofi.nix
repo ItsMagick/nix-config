b9d29646b1f0cbd23225201f5dbc021edb24c6e1
ItsMagick <magick930@googlemail.com>
2026-08-26 11:06:17 +0200

    - added matugen accent colors for rofi

--- modules/rofi.nix
+++ modules/rofi.nix
@@ -1,10 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, config, lib, ... }:
 {
-  programs.rofi = {
-    enable = true;
-    package = pkgs.rofi;
-  };
-  xdg.configFile."rofi/config.rasi".source = ./rofi/config.rasi;
-  xdg.configFile."rofi/theme.rasi".source = ./rofi/theme.rasi;
+  home.packages = [ pkgs.rofi ];
 
+  xdg.configFile."rofi/config.rasi".source = ./rofi/config.rasi;
+  xdg.configFile."rofi/theme.rasi".source  = ./rofi/theme.rasi;
 }
